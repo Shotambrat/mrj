cbc2fd629e91bd850f62db32e49eddb1e2e3d7e1
Abu <dudumikah@gmail.com>
2024-07-16 09:53:02 +0500

    FEAT: CREATE - ABOUT US - initialise

---
+++ app/_components/About/Banner.js
@@ -0,0 +1,8 @@
+
+export default function Banner() {
+  return (
+    <div className="w-full max-w-[1440px]">
+
+    </div>
+  )
+}
---
+++ app/about/page.js
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <main className="w-full bg-white flex flex-col gap-32">
+      
+    </main>
+  );
+}
