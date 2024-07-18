7dcde8ca8551f6176eb4b2b770b634179a27d323
Abu <dudumikah@gmail.com>
2024-07-18 09:42:53 +0500

    Catalog

--- app/_components/Categories/CategoryItem.js
+++ app/_components/Categories/CategoryItem.js
@@ -2,14 +2,14 @@ import Image from "next/image";
 import Link from "next/link";
 import GreenArrow from "@/app/_components/Buttons/GreenArrow";
 
-export default function CategoryItem({ key, title, imageSrc }) {
+export default function CategoryItem({ key, title, imageSrc, slug }) {
   return (
     <div
       key={key}
       className="w-full rounded-3xl bg-slate-100 overflow-hidden px-6 py-6 max-mdx:px-4 max-mdx:py-4 max-mdx:rounded-2xl transition-all duration-200 relative pb-52 max-mdx:h-[300px] flex flex-col gap-5"
     >
       <h2 className="text-3xl max-mdx:text-2xl font-semibold z-10">{title}</h2>
-      <Link href={"/categories"}>
+      <Link href={`/categories/catalog/${slug}`}>
         <GreenArrow title={"view all"} />
       </Link>
       <Image
--- app/categories/page.js
+++ app/categories/page.js
@@ -1,9 +1,14 @@
-import Main from "@/app/_components/Main/Main";
+import Map from "../_components/About/Map";
+import List from "../_components/Categories/List";
+import Application from "../_components/Main/Application";
+
 
 export default function Home() {
   return (
-    <div>
-      <Main />
+    <div className="w-full bg-white flex flex-col gap-44 pt-24">
+      <List />
+      <Application />
+      <Map />
     </div>
   );
 }
