d9fdc7c2ec58ca4b89667c1bcb11dd9d571db4a2
Abu <dudumikah@gmail.com>
2024-07-18 10:15:57 +0500

    FEAT: UPDATE - Catalog

--- app/_components/Catalog/CatalogBar.js
+++ app/_components/Catalog/CatalogBar.js
@@ -1,45 +1,60 @@
 
 
+import React from "react";
+import { Accordion, AccordionItem } from "@nextui-org/react";
+
 export default function CatalogList() {
-    [
+  const data = [
+    {
+      id: 1,
+      name: "Ultrasound Diagnostic Systems",
+      slug: "1-ultrasound-diagnostic-systems",
+      active: true,
+      catalogList: [
         {
-            id: 1,
-            name: "update Ultrasound Diagnostic Systemss",
-            slug: "1-ultrasound-diagnostic-systems",
-            active: true,
-            catalogList: [
-                {
-                    id: 4,
-                    name: "Portable",
-                    slug: "4-portable",
-                    active: true
-                },
-                {
-                    id: 2,
-                    name: "Stationary",
-                    slug: "2-stationary",
-                    active: true
-                }
-            ]
+          id: 4,
+          name: "Portable",
+          slug: "4-portable",
+          active: true,
         },
         {
-            id: 2,
-            name: "Labaratory Equipment",
-            slug: "2-labaratory-equipment",
-            active: true,
-            catalogList: [
-                {
-                    id: 3,
-                    name: "Portable",
-                    slug: "3-portable",
-                    active: true
-                }
-            ]
-        }
-    ]
+          id: 2,
+          name: "Stationary",
+          slug: "2-stationary",
+          active: true,
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: "Laboratory Equipment",
+      slug: "2-laboratory-equipment",
+      active: true,
+      catalogList: [
+        {
+          id: 3,
+          name: "Portable",
+          slug: "3-portable",
+          active: true,
+        },
+      ],
+    },
+  ];
+
   return (
-    <div className='w-full'>
-        
-    </div>
-  )
+    <Accordion selectionMode="multiple">
+      {data.map(
+        (item, i) =>
+          item.active && (
+            <AccordionItem key={item.id} aria-label={item.name} title={item.name}>
+              {item.catalogList &&
+                item.catalogList.map(
+                  (catalogItem) =>
+                    catalogItem.active && <p key={catalogItem.id}>{catalogItem.name}</p>
+                )}
+            </AccordionItem>
+          )
+      )}
+    </Accordion>
+  );
 }
--- app/categories/catalog/[slug]/page.js
+++ app/categories/catalog/[slug]/page.js
@@ -1,9 +1,12 @@
+import CatalogList from '@/app/_components/Catalog/CatalogBar'
 import React from 'react'
 
 export default function page() {
   return (
     <div>
-        Page
+        <div className='w-full max-w-[500px]'>
+          <CatalogList />
+        </div>
     </div>
   )
 }
