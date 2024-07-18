d0913cf94004f89c5ace203b9cfcab7a9dfd1e9e
Andrey <Artemev>
2024-07-18 14:47:08 +0500

    news complete

--- app/_components/News/NewsComp.js
+++ app/_components/News/NewsComp.js
@@ -1,7 +1,10 @@
+"use client"
+
 import newsPhoto from "@/public/images/news/news-photo.png";
 import NewCard from "@/app/_components/News/NewCard";
 import Pagination from "@/app/_components/News/Pagination";
 import Link from "next/link";
+import { useState } from 'react';
 
 export default function NewsComp() {
   const data = [
@@ -29,13 +32,96 @@ export default function NewsComp() {
       imageSrc: newsPhoto,
       slug: "children",
     },
+    {
+      title: "The Future of Telemedicine and Remote Patient Monitoring",
+      date: "12 June",
+      imageSrc: newsPhoto,
+      slug: "telemedicine",
+    },
+    {
+      title: "The Impact of Portable Medical Devices on Healthcare Accessibility",
+      date: "12 June",
+      imageSrc: newsPhoto,
+      slug: "medical-devices",
+    },
+    {
+      title: "The Future of Telemedicine and Remote Patient Monitoring",
+      date: "12 June",
+      imageSrc: newsPhoto,
+      slug: "telemedicine",
+    },
+    {
+      title: "Children's health: Vaccination and prevention of infectious diseases",
+      date: "12 June",
+      imageSrc: newsPhoto,
+      slug: "children",
+    },
+    {
+      title: "The Future of Telemedicine and Remote Patient Monitoring",
+      date: "12 June",
+      imageSrc: newsPhoto,
+      slug: "telemedicine",
+    },
+    {
+      title: "The Impact of Portable Medical Devices on Healthcare Accessibility",
+      date: "12 June",
+      imageSrc: newsPhoto,
+      slug: "medical-devices",
+    },
+    {
+      title: "The Future of Telemedicine and Remote Patient Monitoring",
+      date: "12 June",
+      imageSrc: newsPhoto,
+      slug: "telemedicine",
+    },
+    {
+      title: "Children's health: Vaccination and prevention of infectious diseases",
+      date: "12 June",
+      imageSrc: newsPhoto,
+      slug: "children",
+    },
+    {
+      title: "The Future of Telemedicine and Remote Patient Monitoring",
+      date: "12 June",
+      imageSrc: newsPhoto,
+      slug: "telemedicine",
+    },
+    {
+      title: "The Impact of Portable Medical Devices on Healthcare Accessibility",
+      date: "12 June",
+      imageSrc: newsPhoto,
+      slug: "medical-devices",
+    },
+    {
+      title: "The Future of Telemedicine and Remote Patient Monitoring",
+      date: "12 June",
+      imageSrc: newsPhoto,
+      slug: "telemedicine",
+    },
+    {
+      title: "Children's health: Vaccination and prevention of infectious diseases",
+      date: "12 June",
+      imageSrc: newsPhoto,
+      slug: "children",
+    },
+    
   ];
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 12; // Количество новостей на одной странице
+
+  // Определим индексы новостей, которые нужно отображать на текущей странице
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentItems = data.slice(startIndex, endIndex);
+
+  const totalPages = Math.ceil(data.length / itemsPerPage);
 
   return (
     <div className="w-full max-w-[1440px] mx-auto px-2 flex flex-col gap-8 my-[120px] mdx:my-[200px] 2xl:my-[250px]">
       <h2 className="text-3xl max-mdx:text-2xl font-semibold">NEWS</h2>
       <div className="w-full grid gap-4 grid-cols-1 mdl:grid-cols-2 xl:grid-cols-4 h-auto">
-        {data.map((item, i) => (
+        {currentItems.map((item, i) => (
           <Link key={i} href={`/news/${item.slug}`}>
             <NewCard
               key={i}
@@ -47,7 +133,7 @@ export default function NewsComp() {
         ))}
       </div>
       <div className="flex w-full justify-center">
-        <Pagination />
+        <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={setCurrentPage} />
       </div>
     </div>
   );
--- app/_components/News/Pagination.js
+++ app/_components/News/Pagination.js
@@ -1,52 +1,47 @@
-"use client"
 
-import React, { useState, useEffect } from 'react';
-// import { fetchNews } from '@/app/lib/api';
-import News from "@/app/_components/News/NewCard"
+import React from 'react';
 
-
-export default function Pagination() {
-    const [news, setNews] = useState([]);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [totalPages, setTotalPages] = useState(1);
-
-    // useEffect(() => { 
-    //     const getNews = async () => {
-    //         const data = await fetchNews(currentPage);
-    //         console.log(data)
-    //         setNews(data.data.content);
-    //         setTotalPages(Math.ceil(30 / 12)); 
-    //     };
-    //     getNews();
-    // }, [currentPage]);
-
+export default function Pagination({ currentPage, totalPages, onPageChange }) {
     const handlePageChange = (page) => {
-        setCurrentPage(page);
+        if (page >= 1 && page <= totalPages) {
+            onPageChange(page);
+        }
     };
 
     return (
         <div>
-            <div className="grid grid-cols-1 mdx:grid-cols-2 xl:grid-cols-4 gap-6">
-                {news.map((news) => (
-                    <div key={news.id}>
-                        <News title={news.title} excerpt={news.description} slug={`news/${news.slug}`} imageSrc={news.photoUrl} />
-                    </div>
-                ))}
-            </div>
             <div className="mt-6 flex">
                 <ul className="flex list-none">
+                    <li>
+                        <button
+                            onClick={() => handlePageChange(currentPage - 1)}
+                            disabled={currentPage === 1}
+                            className="px-4 py-2 rounded-3xl mx-1 bg-white text-gray-700"
+                        >
+                            &lt;
+                        </button>
+                    </li>
                     {[...Array(totalPages)].map((_, index) => (
                         <li key={index}>
                             <button
                                 onClick={() => handlePageChange(index + 1)}
-                                className={`px-4 py-2 rounded-3xl mx-1 ${currentPage === index + 1 ? 'bg-greentxt text-white' : 'bg-white text-gray-700'}`}
+                                className={`px-4 text-[16px] mdx:text-[20px] py-2 rounded-3xl mx-1 ${currentPage === index + 1 ? 'bg-greentxt text-white' : 'bg-white text-gray-400'}`}
                             >
                                 {index + 1}
                             </button>
                         </li>
                     ))}
+                    <li>
+                        <button
+                            onClick={() => handlePageChange(currentPage + 1)}
+                            disabled={currentPage === totalPages}
+                            className="px-4 py-2 rounded-3xl mx-1 bg-white text-gray-700"
+                        >
+                            &gt;
+                        </button>
+                    </li>
                 </ul>
             </div>
         </div>
     );
-};
+}
