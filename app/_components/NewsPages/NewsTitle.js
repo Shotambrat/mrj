eb5e3fe54a264976d25ce84ec765ecf8db99a6d8
Andrey <Artemev>
2024-07-19 12:21:38 +0500

    complete newspages

--- app/_components/NewsPages/NewsTitle.js
+++ app/_components/NewsPages/NewsTitle.js
@@ -1,19 +1,18 @@
 import Image from "next/image"
 import newsPhoto from "@/public/images/news/news-photo.png";
-import OtherNews from "@/app/_components/NewsPages/OtherNews";
 
 export default function NewsTitle() {
     return (
-        <div className="w-full max-w-[1440px] mx-auto flex flex-col  gap-1 px-2 ">
-            <div className=" mt-4">
-                <p className="text-gray-400 text-[16px] mdx:text-[18px]">12 June</p>
-                <h1 className="text-[25px] text-black mb-2 mdx:text-[34px]">Advances in Medical Imaging Technology</h1>
+        <div className="w-full max-w-[832px] mx-auto flex flex-col  gap-1 px-4 ">
+            <div className=" mt-4 ">
+                <p className="text-gray-400 text-[16px] mdx:text-[18px] xl:text-[20px]">12 June</p>
+                <h1 className="text-[25px] text-black mb-2 mdx:text-[34px] xl:text-[40px] ">Advances in Medical Imaging Technology</h1>
             </div>
             <div >
                 <p className="text-[16px] mdx:text-[20px]">Recent developments in medical imaging technology are transforming diagnostic capabilities and patient outcomes. According to a recent article in The Lancet, innovations such as AI-driven imaging analysis and high-resolution ultrasound are significantly enhancing the accuracy and speed of diagnosis. AI algorithms can now detect anomalies in imaging results that might be overlooked by the human eye, thereby improving early detection rates of diseases like cancer. High-resolution ultrasound, on the other hand, is providing clearer images of soft tissues, which is crucial for diagnosing conditions like liver fibrosis and cardiovascular diseases.
                     Furthermore, The Lancet reports that integrating these technologies into standard medical practice requires robust training programs for medical professionals to ensure they can effectively utilize these tools. The potential of these technologies to revolutionize healthcare is immense, promising not only better diagnostic accuracy but also more personalized patient care.</p>
             </div>
-            <div >
+            <div className="xl:mt-7 xl:mb-[80px] flex flex-row justify-center">
                 <Image
                     src={newsPhoto}
                     width={500}
@@ -31,9 +30,9 @@ export default function NewsTitle() {
                 <ol className="list-decimal list-inside">
                     <li className="text-[20px] mdx:text-[27px]">Artificial Intelligence (AI) and Machine Learning</li>
                     <p className="text-[16px] mdx:text-[20px]">AI and machine learning algorithms are revolutionizing medical imaging by enabling faster and more accurate analysis of images. These technologies assist radiologists in detecting abnormalities, such as tumors or fractures, with greater precision and efficiency. AI-powered imaging tools are also capable of predicting patient outcomes and aiding in personalized treatment planning</p>
-                    <li className="text-[20px] mt-[60px]">High-Resolution Imaging</li>
+                    <li className="text-[20px] mdx:text-[27px] mt-[60px]">High-Resolution Imaging</li>
                     <p className="text-[16px] mdx:text-[20px]">AI and machine learning algorithms are revolutionizing medical imaging by enabling faster and more accurate analysis of images. These technologies assist radiologists in detecting abnormalities, such as tumors or fractures, with greater precision and efficiency. AI-powered imaging tools are also capable of predicting patient outcomes and aiding in personalized treatment planning</p>
-                    <div className="mt-[30px] mb-[10px]">
+                    <div className="mt-[30px] mb-[10px] flex flex-row justify-center">
                         <Image
                             src={newsPhoto}
                             width={500}
@@ -44,7 +43,7 @@ export default function NewsTitle() {
                     </div>
                     <li className="text-[20px] mt-[60px] mdx:text-[27px]">3D and 4D Imaging</li>
                     <p className="text-[16px] mdx:text-[20px]">AI and machine learning algorithms are revolutionizing medical imaging by enabling faster and more accurate analysis of images. These technologies assist radiologists in detecting abnormalities, such as tumors or fractures, with greater precision and efficiency. AI-powered imaging tools are also capable of predicting patient outcomes and aiding in personalized treatment planning</p>
-                    <div className="mt-[30px] mb-[10px]">
+                    <div className="mt-[30px] mb-[10px] flex flex-row justify-center">
                         <Image
                             src={newsPhoto}
                             width={500}
--- app/_components/NewsPages/Share.js
+++ app/_components/NewsPages/Share.js
@@ -4,29 +4,31 @@ import Link from "next/link";
 
 export default function NewsTitle() {
     return (
-        <div className="max-w-[1440px] mx-[15px] mdl:mx-auto">
-            <div className="bg-green-100 rounded-3xl mdl:flex mdl:flex-row-reverse mdl:items-center mdl:justify-between">
-                <div className="mb-3 mdl:flex-1 mdl:items-center mdl:flex mdl:my-3">
+        <div className="max-w-[1440px] mx-[15px]  mdl:mx-auto bg-slate-100 rounded-3xl">
+            <div className=" rounded-3xl mdl:flex mdl:flex-row-reverse mdl:items-center mdl:justify-between   relative ">
+                <div className="mdl:items-center mdl:flex mdl:my-3 flex justify-center content-center">
                     <Image
                         src={newsPhoto}
                         width={500}
                         height={500}
                         alt={`News Image`}
-                        className="w-full h-auto max-w-[560px] max-h-[308px] object-contain rounded-3xl mdl:max-h-[383px] mdl:max-w-[383px]"
+                        className=" w-[92%] h-auto max-w-[560px] max-h-[308px] mdl:w-full rounded-3xl mdl:max-h-[383px] mdl:max-w-[383px] mdl:mr-5"
                     />
                 </div>
-                <div className="flex flex-col uppercase text-greentxt mx-4 mdl:flex-1">
-                    <h2 className="font-semibold text-[27px]">
+                <div className="flex flex-col uppercase text-greentxt  mdl:flex-1 ">
+                    <h2 className="z-20 mt-4 ml-3 font-semibold text-[27px] mdl:text-[30px] xl:text-[40px] xl:mb-3 xl:ml-9">
                         Share this news<br /> with a friend!
                     </h2>
-                    <p className="text-gray-500 text-[15px] normal-case mdl:w-[300px]">
+                    <p className="text-gray-500 text-[15px] normal-case mdl:w-[320px] xl:text-[20px] xl:w-[503px] z-20 xl:ml-9 ml-3">
                         Learn more about the latest advances in medical imaging! Share this news with friends and colleagues who may be interested
                     </p>
-                    <Link href="/">
-                        <button className="px-[76px] w-[220px] py-4 mt-[20px] mb-[20px] rounded-2xl bg-greentxt text-white font-semibold">
+                    <div className="semicircle h-[283px] w-full xl:h-full mdl:h-full mdl:w-[50%] xl:w-[50%] rounded-b-2xl rounded-t-[20px] mdl:rounded-r-[140px] "></div>
+                    <Link href="/news">
+                        <button className="ml-3 px-[76px] w-[220px] py-4 mt-[20px] mb-[20px] rounded-2xl bg-greentxt text-white font-semibold xl:mt-[25px] xl:ml-9 z-50 relative">
                             Share
                         </button>
                     </Link>
+
                 </div>
             </div>
         </div>
