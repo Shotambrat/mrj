e4ef52cd82f6f7fd2dfe62bccfacff060a7ca85f
Abu <dudumikah@gmail.com>
2024-07-31 18:37:06 +0500

    Search: Verstka

--- app/_components/Modal/Search.js
+++ app/_components/Modal/Search.js
@@ -1,69 +1,31 @@
-
-
 export default function Search() {
   return (
-    <div className="fixed h-screen w-full bg-modalBg ">
-        <div className="w-full bg-white h-[80%]">
-            <div className="w-full max-w-[1440px] mx-auto px-2 flex flex-col gap-8 pt-8">
+    <div className="fixed h-screen w-full bg-modalBg left-0 top-[100px] z-[9999]">
+      <div className="h-[70%] w-full bg-white pt-8">
+        <div className="h-full w-full max-w-[1440px] mx-auto flex flex-col gap-8">
+          <div className="flex items-center bg-snowy rounded-lg p-4 w-full shadow-sm">
             <input
-                  type={field === "email" ? "email" : "text"}
-                  name={field}
-                  value={values[field]}
-                  onChange={handleInputChange}
-                  onFocus={() => setFocusedInput(field)}
-                  onBlur={() => setFocusedInput(null)}
-                  className={`block w-full px-3 py-3 bg-white rounded-lg shadow-sm placeholder-transparent focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm border-2 ${
-                    focusedInput === field
-                      ? validateInput(field, values[field]).isValid
-                        ? "border-green-500"
-                        : "border-red-500"
-                      : "border-transparent"
-                  }`}
-                  placeholder={
-                    field === "fullName"
-                      ? "Full name"
-                      : field === "phoneNumber"
-                      ? "Phone number"
-                      : field === "email"
-                      ? "E-mail"
-                      : "Your question"
-                  }
-                />
-                <label
-                  htmlFor={field}
-                  className={`absolute left-3  transition-all ${
-                    focusedInput === field || values[field]
-                      ? "-top-4 text-xs"
-                      : "top-3 text-sm"
-                  } ${
-                    focusedInput === field
-                      ? validateInput(field, values[field]).isValid
-                        ? "text-green-500"
-                        : "text-red-500"
-                      : "text-gray-400"
-                  } cursor-text`}
-                  onClick={() => document.getElementsByName(field)[0].focus()}
-                >
-                  {focusedInput === field && values[field].length > 0 ? (
-                    validateInput(field, values[field]).message
-                  ) : field === "fullName" ? (
-                    <p>
-                      Full Name
-                      <span className="text-red-600 ml-2">*</span>
-                    </p>
-                  ) : field === "phoneNumber" ? (
-                    <p>
-                      Phone number
-                      <span className="text-red-600 ml-2">*</span>
-                    </p>
-                  ) : field === "email" ? (
-                    "E-mail"
-                  ) : (
-                    "Your question"
-                  )}
-                </label>
-            </div>
+              type="text"
+              placeholder="Enter the name of the product, page, etc..."
+              className="bg-transparent outline-none flex-1 text-gray-600 placeholder-gray-400"
+            />
+            <svg
+              className="w-5 h-5 text-greenView"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              ></path>
+            </svg>
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
